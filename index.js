7bb444e4ebd1e25c85ed695ab8b7df7a4f41c78d
zerochen2016 <470133773@qq.com>
2019-11-23 16:59:13 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+    socket.on('chat message', function(msg){
+        console.log('message:' + msg)
+        io.emit('chat message',msg);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(port, function(){
+    console.log('listening on *: 3000');
+});
